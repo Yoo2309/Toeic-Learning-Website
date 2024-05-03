23dd8b9ed0960f4e2f2f7cd350c21eced1a79c6a
Yoo2309 <dutran2392002@gmail.com>
2024-05-04 05:24:17 +0700

    feat-comment-comment-ui map

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./src/Component/Profile/UserProfile.jsx",
     "./src/Component/Profile/ChangePassword.jsx",
+    "./src/Component/Courses/Lessons/Comment/Comment.jsx",
+    "./src/Component/Courses/Lessons/Comment/CommentItem.jsx",
   ],
   theme: {
     extend: {},
